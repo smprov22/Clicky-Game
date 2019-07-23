a5ab302ed3300308cdc4f815b18cdc2aed1f3ca3
smprov22 <smprovence22@gmail.com>
2019-07-22 22:45:10 -0700

    styling mostly done

--- clicky-game/src/components/Title/index.js
+++ clicky-game/src/components/Title/index.js
@@ -6,7 +6,7 @@ function Title(props) {
     <div className="jumbotron jumbotron-fluid">
       <div className="container">
         <h1 className="title display-4">{props.children}</h1>
-        <p className="lead">Try and keep track of all the Weasley's!</p>
+        <p className="lead subtitle">Try and keep track of all the Weasley's! Click on an image to earn points, but don't click on any more than once!</p>
       </div>
       <div className="container">
         <p className="guessOutcome">You guessed (in)correctly!</p>
