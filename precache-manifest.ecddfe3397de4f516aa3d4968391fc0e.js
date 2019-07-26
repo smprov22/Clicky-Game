0d534f8f113e5dd0d00fec66ca85597a828798c3
smprov22 <smprovence22@gmail.com>
2019-07-26 13:45:38 -0700

    Updates

---
+++ precache-manifest.ecddfe3397de4f516aa3d4968391fc0e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9ae8a058e45fd21ed6e13749af8cabc",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "1d4e75b21bc02c57c47f",
+    "url": "/Clicky-Game/static/css/main.658c8827.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/Clicky-Game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "1d4e75b21bc02c57c47f",
+    "url": "/Clicky-Game/static/js/main.9d49727c.chunk.js"
+  },
+  {
+    "revision": "fab82da4d9420c34bc57",
+    "url": "/Clicky-Game/static/js/runtime~main.012bace8.js"
+  }
+]);
